f3b1ffc41230504370c91047eb9d51317456ebda
T-Bosma <taitwando@gmail.com>
2019-12-02 12:35:32 +0100

    Reordered href's in header

--- include/header.php
+++ include/header.php
@@ -2,11 +2,11 @@
 <html lang="nl">
 	<head>
 		<title>Sollestijn</title>
+		<link href="stylesheets/bootstrap.css" type="text/css" rel="stylesheet">
+		<link href="stylesheets/style.css" type="text/css" rel="stylesheet">
 		<?php
 		echo "<link href='stylesheets/".$stylesheet.".css' type='text/css' rel='stylesheet'>";
 		?>
-		<link href="stylesheets/bootstrap.css" type="text/css" rel="stylesheet">
-		<link href="stylesheets/style.css" type="text/css" rel="stylesheet">
 		<meta name="description" content="Sollestijn">
 		<meta charset="UTF-8">
 		<meta name="author" content="Twan Bosma">
