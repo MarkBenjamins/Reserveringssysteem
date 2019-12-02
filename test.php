cb9417a87285e8c16d6042685ff1d014ce3a8d9f
Wesley <wj.schoonbeek@gmail.com>
2019-12-02 12:59:51 +0100

    dsada
    dsadas

--- test.php
+++ test.php
@@ -3,15 +3,32 @@ $stylesheet = "mycustomsheet.css";
 include('include/header.php');
 ?>
 
+<?php
+
+$connection = mysqli_connect("localhost", "root","", "sollestijn");
+
+if(mysqli_connect_errno()) {
+    die("Could not connect to the database.."); //echo mysqli_connect_errno();
+} else {
+
+    $sql = mysqli_query($connection,"SELECT * FROM administrator");
+    if($sql) {
+        
+    } else {
+        echo "no";
+    }
+}
+
+?>
+
+
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-6">
-            x
             Voorbeeld van hoe rows werken
         </div>
         <div class="col-6">
-            x
             Voorbeeld van hoe rows werken
         </div>
     </div>
