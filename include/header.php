be97c053910a9c4331b2c53cf564bfd2178586bc
T-Bosma <taitwando@gmail.com>
2019-12-02 11:04:54 +0100

    added basics
    added basic stuff

---
+++ include/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<title>Sollestijn</title>
+		<link href="css/style.css" type="text/css" rel="stylesheet">
+		<meta name="description" content="Sollestijn">
+		<meta charset="UTF-8">
+		<meta name="author" content="Twan Bosma">
+	</head>
+	<body>
+		
+	</body>
+</html>
