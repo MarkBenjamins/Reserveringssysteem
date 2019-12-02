cfe2e8a7828d4dc71f1f75a8f9bf285c2c46322e
Wesley <wj.schoonbeek@gmail.com>
2019-12-02 12:20:48 +0100

    Updated header.php
    dsa

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include('include/header.php');
+?>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-6">
+            x
+        </div>
+        <div class="col-6">
+            x
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+include('include/footer.php');
+?>
