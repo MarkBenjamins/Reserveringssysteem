0b444b40eceac16a8aec66f8bd4a91ada17fc307
ramonjgw <56031483+ramonjgw@users.noreply.github.com>
2019-12-02 12:34:20 +0100

    betaling

--- reservering/res-betaling.php
+++ reservering/res-betaling.php
@@ -1,5 +1,11 @@
 <?php include "../include/header.php"; ?>
 
-
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            
+        </div>
+    </div>
+</div>
 
 <?php include "../include/footer.php"; ?>
