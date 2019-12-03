1ca79dd5e5d2d9e7f9bb687e2217b9b5ac2a65d7
Wesley <wj.schoonbeek@gmail.com>
2019-12-03 12:49:35 +0100

    Header update
    dsa

--- include/header.php
+++ include/header.php
@@ -14,7 +14,30 @@
 	</head>
 	<body>
 		<div class="container-fluid">
+			<div class="row">
+				<div id="main" class="col-lg-8 col-12">
+					<div id="header" class="row">
+						<div id="logo" class="col-3">
+							<a href="index.php"><img src="img/logo.png" alt="logo"></a>
 						</div>
+						<div id="span" class="col-9">
+						<div class="row">
+							<div id="banner" class="col-12">
+									<p class="h1">Landgoed Sollestijn</p>
+								</div>
+								<div id="nav" class="col-12">
+									<nav class="navbar-expand-lg">
+										<ul class="nav nav-pills nav-justified">
+											<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Reserveren</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Gallerij</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+										</ul>
+									</nav>
+								</div>
+							</div>
 						</div>
 					</div>
+				
+				
 				<!-- Dit is onze MAIN CONTENT !! -->
