96d86ae7a596b2436fab4aa215303c52e6e7b47f
jurrienvandervelde <56027041+jurrienvandervelde@users.noreply.github.com>
2019-12-09 11:12:14 +0100

    Update gallery.php
    includes verwijderd

--- gallery.php
+++ gallery.php
@@ -9,15 +9,6 @@
     <title>Gallerij</title>
   </head>
   <body>
-  <?php
-  $stylesheet = "mycustomsheet";
-  include('include/header.php');
-?>
-
-<?php
-include('include/footer.php');
-?>
-
     <h1>Gallerij</h1>
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
