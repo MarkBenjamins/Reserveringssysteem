588aabc10318b0bcaefbbd9e81d436e1e2e8a5ae
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2019-12-09 16:24:03 +0100

    update

--- res-home.php
+++ res-home.php
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 <?php
 $stylesheet = "res-home";
 include('include/header.php');
@@ -11,19 +5,37 @@ include('include/header.php');
 
 
 <!--Start Kamer Keuze Menu-->
-    <div>
-	<img src="../img/Voorbeeldkeuzemenu.png" alt="voorbeeldmenu">   
-        <form action="/action_page.php">
-            Aankomst: <input type="date" name="Aankomst"class="btn btn-outline-danger">
-            Vertrek: <input type="date" name="Vertrek"class="btn btn-outline-danger">
-            <select name="kamerkeuze" class="btn btn-outline-danger">
-                <option value="Eenpersoonskamer">Eenpersoonskamer</option>
-                <option value="Tweepersoonskamer">Tweepersoonskamer</option>
-                <option value="Vierpersoonskamer">Vierpersoonskamer</option>
-            </select>
-            <button type="button" class="btn btn-outline-danger">Zoek</button>
-        </form>
-    </div>
+  <center><h1>Reservering</h1></center>
+  <div>
+    <div id="res-home"><center>
+		<form class="dd-mm-jjjj">
+			<div>
+				Aankomst: 
+			</div>
+			<div class="margin">
+				<input type="date" name="Aankomst"class="btn btn-outline-danger">
+			</div>
+			<div>
+				Vertrek: 
+			</div>
+			<div class="margin">
+				<input type="date" name="Aankomst"class="btn btn-outline-danger">
+			</div>
+			<div class="kamer">
+				<div>
+					Kamerkeuze: 
+				</div>			
+				<select name="kamerkeuze" class="btn btn-outline-danger">
+					<option value="Eenpersoonskamer">Eenpersoonskamer</option>
+					<option value="Tweepersoonskamer">Tweepersoonskamer</option>
+					<option value="Vierpersoonskamer">Vierpersoonskamer</option>
+				</select>
+			</div>
+				<div>
+				<button type="button" class="btn btn-outline-danger">Zoek</button>
+				</div>
+		</form>
+	</div></center>
 <!--End Kamer Keuze Menu-->
 
 
