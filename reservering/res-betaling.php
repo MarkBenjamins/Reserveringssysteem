38496ec3c20dc7dce68e8d5d259401765ad0effb
ramonjgw <56031483+ramonjgw@users.noreply.github.com>
2019-12-09 10:07:56 +0100

    Update res-betaling.php

--- reservering/res-betaling.php
+++ reservering/res-betaling.php
@@ -1,9 +1,17 @@
-<?php include "../include/header.php"; ?>
+<?php 
+$stylesheet = "betalen.css";
+include "../include/header.php"; 
+?>
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col">
-            
+        <div class="col-3">
+        </div>
+        <div class="col-6">
+            <p>test</p>
+            <h1>dead</h1>
+        </div>
+        <div class="col-3">
         </div>
     </div>
 </div>
