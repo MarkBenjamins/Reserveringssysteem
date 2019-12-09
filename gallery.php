46c616183ebfed105f21dbd65b96407a5d349122
jurrienvandervelde <56027041+jurrienvandervelde@users.noreply.github.com>
2019-12-09 11:37:58 +0100

    Update gallery.php
    d-block rezised

--- gallery.php
+++ gallery.php
@@ -12,13 +12,13 @@ include('include/header.php');
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="img/kamer1.jpg" alt="First slide">
+      <img class="d-block w-70" src="img/kamer1.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="img/kamer2.jpg" alt="Second slide">
+      <img class="d-block w-70" src="img/kamer2.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="img/kamer3.jpg" alt="Third slide">
+      <img class="d-block w-70" src="img/kamer3.jpg" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
