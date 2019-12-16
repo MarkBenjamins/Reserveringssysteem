c6be4a8e17ee4b6536d9982f08fc85f09fb2f3eb
Wesley <wj.schoonbeek@gmail.com>
2019-12-16 12:32:43 +0100

    Inlog systeme

--- inlog.php
+++ inlog.php
@@ -10,7 +10,9 @@ include('include/header.php');
                 </div>
                 <div class="col-8">
                     <p class="title">Voer hier uw inloggegevens in</p>
+                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
                         <p>Gebruikersnaam:</p>
+                        <input class="form" type="text" name="uname" placeholder="Gebruikersnaam"><br><br>
                         <p>Wachtwoord:</p>
                         <input class="form" type="password" name="pword" placeholder="Wachtwoord"><br><br>
                         <input type="submit" name="submit" value="Log in">
@@ -24,3 +26,53 @@ include('include/header.php');
 </div>
 <?php
 include('include/footer.php');
+
+
+if(ISSET($_POST["submit"])) {
+    if(!empty($_POST["pword"]) || !empty($_POST["uname"])) {
+
+        
+        $uname = $_POST["uname"];
+        $pword = $_POST["pword"];
+        $conn = mysqli_connect("localhost","root","","sollestijn");
+
+
+        $sql = "SELECT * FROM gebruikers WHERE `gebruikersnaam` = ?";
+
+        if($stmt = mysqli_prepare($conn, $sql)) {
+
+            /* bind parameters for markers */
+            mysqli_stmt_bind_param($stmt, "s", $uname);
+
+            /* execute query */
+            mysqli_stmt_execute($stmt);
+
+            /* bind result variables */
+            mysqli_stmt_bind_result($stmt, $id, $gebruikersnaam, $wachtwoord, $rechten);
+
+            /* fetch value */
+            $fetch = mysqli_stmt_fetch($stmt);
+
+            /* close statement */
+            mysqli_stmt_close($stmt);
+
+            if($wachtwoord == $pword) {
+                
+                $_SESSION["gebruiker"]["id"] = $id;
+                $_SESSION["gebruiker"]["rechten"] = $rechten;
+                $_SESSION["gebruiker"]["gebruikersnaam"] = $gebruikersnaam;
+            }
+        } else {
+            die("could not prepare");
+        }
+
+
+
+
+    } else {
+        $_SESSION["messages"] = "Vul gelieve beide velden in !";
+        echo "empty fields";
+    }
+}
+?>
+
