0d58907f40940ef929cfc0ca928c63160093c496
Wesley <wj.schoonbeek@gmail.com>
2019-12-20 14:31:35 +0100

    Changed gallery image
    Gallerij bestand aangepast zodat jurrien zijn afbeelding klopt

--- gallery.php
+++ gallery.php
@@ -2,73 +2,75 @@
 $stylesheet = "gallery";
 include('include/header.php');
 ?>
-<div id="gallerybox"> <!-- start gallerybox !-->
-  <div id="galleryheader"><center><h1>Gallerij</h1></center>
+<div id="gallerybox">
+  <!-- start gallerybox !-->
+  <div id="galleryheader">
+    <center>
+      <h1>Gallerij</h1>
+    </center>
   </div>
-<?php
-if(isset($_REQUEST['submit']))
-{
-  $filename=  $_FILES["imgfile"]["name"];
-  if ((($_FILES["imgfile"]["type"] == "image/gif")|| ($_FILES["imgfile"]["type"] == "image/jpeg") || ($_FILES["imgfile"]["type"] == "image/png")  || ($_FILES["imgfile"]["type"] == "image/pjpeg")) && ($_FILES["imgfile"]["size"] < 200000))
-  {
-    if(file_exists($_FILES["imgfile"]["name"]))
-    {
-      echo "File name exists.";
-    }
-    else
-    {
-      move_uploaded_file($_FILES["imgfile"]["tmp_name"],"img/$filename");
-      echo "Upload Successful . <a href='img/$filename'>Click here</a> to view the uploaded image";
+  <?php
+  if (isset($_REQUEST['submit'])) {
+    $filename =  $_FILES["imgfile"]["name"];
+    if ((($_FILES["imgfile"]["type"] == "image/gif") || ($_FILES["imgfile"]["type"] == "image/jpeg") || ($_FILES["imgfile"]["type"] == "image/png")  || ($_FILES["imgfile"]["type"] == "image/pjpeg")) && ($_FILES["imgfile"]["size"] < 200000)) {
+      if (file_exists($_FILES["imgfile"]["name"])) {
+        echo "File name exists.";
+      } else {
+        move_uploaded_file($_FILES["imgfile"]["tmp_name"], "img/$filename");
+        echo "Upload Successful . <a href='img/$filename'>Click here</a> to view the uploaded image";
+      }
+    } else {
+      echo "invalid file.";
     }
   }
-  else
-  {
-    echo "invalid file.";
-  }
-}
 
-if(isset($_SESSION["gebruiker"])) {
-  //var_dump($_SESSION["gebruiker"]);
   ?>
+  <div class="row">
+    <div id="carouselExampleIndicators" class="carousel slide col-12" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/kamer1.jpg" alt="first">
+        </div>
+        <div class="carousel-item">
+          <img src="img/kamer2.jpg" alt="second">
+        </div>
+        <div class="carousel-item">
+          <img src="img/kamer3.jpg" alt="third">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div> <!-- eind gallerybox !-->
+  </div>
 
-<form method="post" enctype="multipart/form-data">
+
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</div>
+
+<?php
+if (isset($_SESSION["gebruiker"])) {
+  //var_dump($_SESSION["gebruiker"]);
+
+  echo '<form method="post" enctype="multipart/form-data">
   File name:<input type="file" name="imgfile">
   <input type="submit" name="submit" value="upload">
-</form>
-
-<?php
+</form>';
 }
-?>
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="img/kamer1.jpg" alt="first">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="img/kamer2.jpg" alt="second">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="img/kamer3.jpg" alt="third">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-  </div> <!-- eind gallerybox !-->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</div>
-<?php
+
 include('include/footer.php');
 ?>
--- res-home.php
+++ res-home.php
@@ -5,28 +5,31 @@ include('include/header.php');
 	<!--Door Mark Benjamins-->
 <!--Start Kamer Keuze Menu-->
     <center><h1>Reservering</h1></center>
-		<form id="res-home" action="res-betaling.php" method="POST"><center>
+		<form id="res-home" action="res-home-validation.php" method="POST"><center>
 				Aankomst: 
 			<div>	<!-- Datum keuze menu -->
-				<input type="date" name="Aankomst" class="btn btn-outline-danger">
+				<input required type="date" min="<?php echo date("Y-m-d"); ?>" name="aankomst" class="btn mark-btn">
 			</div>
 				Vertrek: 
 			<div>	<!-- Datum keuze menu -->
-				<input type="date" name="Aankomst" class="btn btn-outline-danger">
+				<input required type="date" name="vertrek" class="btn mark-btn">
 			</div>
 				Kamerkeuze: 
 			<div>	<!-- Kamer keuze menu -->			
-				<select name="kamerkeuze" class="btn btn-outline-danger">
-					<option value="Eenpersoonskamer">Eenpersoonskamer</option>
-					<option value="Tweepersoonskamer">Tweepersoonskamer</option>
-					<option value="Vierpersoonskamer">Vierpersoonskamer</option>
+				<select name="kamerkeuze" class="btn mark-btn">
+					<option value="1">Eenpersoonskamer</option>
+					<option value="2">Tweepersoonskamer</option>
+					<option value="3">Vierpersoonskamer</option>
 				</select> 
 			</div>
 			<div>	<!-- Zoek knop -->
-				<input type="submit" class="btn btn-outline-danger" value="Zoek">
+				<input required type="submit" class="btn btn-outline-danger" name="submit" value="Zoek">
 			</div>
 		</form></center>
 <!--End Kamer Keuze Menu-->
+
+
 <?php
+
 include('include/footer.php');
 ?>
