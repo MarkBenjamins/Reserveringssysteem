a6f9e714003cfab8d62236520dd1fca3a6681f91
jurrienvandervelde <56027041+jurrienvandervelde@users.noreply.github.com>
2020-01-06 09:37:02 +0100

    Update gallery.php
    deleted white lines

--- gallery.php
+++ gallery.php
@@ -56,7 +56,6 @@ include('include/header.php');
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </div>
-
 <?php
 if (isset($_SESSION["gebruiker"])) {
     //var_dump($_SESSION["gebruiker"]);
@@ -66,6 +65,5 @@ if (isset($_SESSION["gebruiker"])) {
   <input type="submit" name="submit" value="upload">
 </form>';
 }
-
 include('include/footer.php');
 ?>
