f7d4812c3bd0064067a9eaf0d0b1f08f3eabe15e
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-01-10 20:40:18 +0100

    Begin gegevens validatie
    begonnen met het valideren van de input.
    - alle verplichte velden verplicht.
    - naam mag alleen bestaan uit ....
    - is email een valid email

--- res-gegevens.php
+++ res-gegevens.php
@@ -13,7 +13,46 @@ include('include/header.php');
                 <div class="col-8">
                     <h1>Persoonsgegevens</h1>
                     <center>
-                        <form action="<?php echo $_SERVER['res-factuur.php']; ?>" method="POST">
+                        <?php
+                        //Door Mark Benjamins
+                        if (isset($_POST['submit'])) { //alle inpute naar var
+                            $fname = $_POST['fname'];
+                            $lname = $_POST["lname"];
+                            $email = $_POST["email"];
+                            $tel = $_POST["tel"];
+//                            $post = $_POST["post"];
+//                            $hnummer = $_POST["hnummer"];
+//                            $gdate = $_POST["gdate"];
+//                            $ltijd = $_POST["ltijd"];
+//                            $ontbijt = $_POST["ontbijt"];
+//                            $lunch = $_POST["lunch"];
+//                            $diner = $_POST["diner"];
+//                            $efiets = $_POST["efiets"];
+//                            $epaal = $_POST["epaal"];
+//                            $korting = $_POST["korting"];
+//                            $opmerk = $_POST["opmerk"];
+            
+                            if (empty($fname or $lname or $email or $tel or $post or $hnummer or $gdate)) {
+                                echo "Niet alle verplichte velden zijn ingevuld";
+                                // Iets niet ingevuld error
+                            }
+                            if (!preg_match('/[-_ a-zA-Z0-9]/i', $fname or $lname or $tel or $post)) {
+                                // E-mail staat er niet bij, die bevat namelijk speciale karakters
+                                // preg_match geeft een true or false waarde
+                                // Karakters anders dan a-z, A-Z, 0-9, spartie, _ en - zijn niet toegestaan 
+                                echo "Het formulier bevat een karakter dat niet is toegestaan.<br>";
+                                echo "Alleen Alfabetiese karakters, getallen, spaties, _ en - zijn toegestaan.<br><br>";
+                            }
+                            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                                echo $email . " is geen valide email adres";
+                                // Als mailadres niet goed is echo foutmelding
+                            } else {
+                                echo "Alles werkt";
+                            }
+							// mederijziges = nummeric bespreek met docent of dit misschien niet hoeft?
+                        }
+                        ?>
+                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
                             <label for="fname">*Voornaam:</label> <!-- add by Mark-->				
                             <div> <!--Voornaam-->
                                 <input required class="formulier" type="text" placeholder=" Uw voornaam" name="fname">
@@ -33,54 +72,59 @@ include('include/header.php');
                             <div> <!--Tel-->
                                 <input required class="formulier" type="tel" placeholder=" Uw telefoonnummer" name="tel">
                             </div>
-							
-                            <label for="adres">*Adres:</label>
-                            <div> <!--Adres-->
-                                <input required class="formulier" type="text" placeholder=" Uw huisnummer" name="adres">
+
+                            <label for="adres">*Postcode:</label>
+                            <div> <!--Postcode-->
+                                <input required class="formulier" type="text" placeholder=" Uw postcode" name="post">
                             </div>
-                            <div>
-                                <input required class="formulier" type="text" placeholder=" Uw postcode" name="adres">
+
+                            <label for="adres">*Huisnummer:</label>
+                            <div> <!--Huisnummer-->
+                                <input required class="formulier" type="text" placeholder=" Uw huisnummer" name="hnummer">
                             </div>
                             <label for="gdate">*Geboortedatum:</label>				
-                            <div> <!--geboortedatum / php maakt het onmogelijk om in de toekomst geboren te zijn.-->	
-                                <input required class="formulier" max="<?php echo date("Y-m-d"); ?>" type="date" name="gdate"><br><br>
+                            <div> <!--Geboortedatum-->	
+                                <input required class="formulier" max="<?php echo date("Y-m-d"); //Onmogelijk om in de toekomst geboren te zijn.  ?>" type="date" name="gdate"><br><br>
                             </div>
                             <label for="leeftijd">Leeftijd van uw medereizigers:</label>
                             <div>
-                                <input class="formulier" type="number" placeholder=" Medereiziger 1" name="leeftijd">
+                                <input class="formulier" type="number" placeholder=" Medereiziger 1" name="ltijd">
                             </div>
                             <div>
-                                <input class="formulier" type="number" placeholder=" Medereiziger 2" name="leeftijd">
+                                <input class="formulier" type="number" placeholder=" Medereiziger 2" name="ltijd">
                             </div>
                             <div>
-                                <input class="formulier" type="number" placeholder=" Medereiziger 3" name="leeftijd">
+                                <input class="formulier" type="number" placeholder=" Medereiziger 3" name="ltijd">
                             </div>
                             <label for="extra">Extra opties:</label>
                             <div>
-                                <input class="formulier" type="checkbox" name="Ontbijt" checked="checked">Ontbijt
+                                <input class="formulier" type="checkbox" name="ontbijt" checked="checked"><label> Ontbijt</label>
                             </div>
                             <div>
-                                <input class="formulier" type="checkbox" name="Lunch">Lunch
+                                <input class="formulier" type="checkbox" name="lunch"><label>Lunch</label>
                             </div>
                             <div>
-                                <input class="formulier" type="checkbox" name="Diner">Diner
+                                <input class="formulier" type="checkbox" name="diner"><label>Diner</label>
+								<br><br>
+                            </div>
+							<label for="e-res">Extra reserverings opties:</label>
+                            <div>
+                                <input class="formulier" type="checkbox" name="efiets"><label>Elektrische fiets</label>
                             </div>
                             <div>
-                                <input class="formulier" type="checkbox" name="E-fiets">Elektrische fiets
-                            </div>
-                            <div>
-                                <input class="formulier" type="checkbox" name="E-paal">Elektrische laadpaal
+                                <input class="formulier" type="checkbox" name="epaal"><label>Elektrische laadpaal</label>
+								<br><br>
                             </div>
                             <label for="korting">Kortingscode:</label>
                             <div>
-                                <input class="formulier" type="text" placeholder=" bijv. A12345" name="korting">
+                                <input class="formulier" type="text" placeholder=" Bijv. A12345" name="korting">
                             </div>
                             <label for="opmerking">Opmerkingen:</label>
                             <div>
-                                <textarea class="formulier" rows="5" cols="20" placeholder="Opmerkingen"></textarea>
+                                <textarea class="formulier" rows="5" cols="20" placeholder="Opmerkingen" name="opmerk" ></textarea>
                             </div>
                             <div> <!--Verzend-->	
-                                <input required class="btn mark-btn" type="submit" name="submit" value="Verzend">
+                                <input required class="btn mark-btn" type="submit" name="submit" value="Submit">
                             </div>
                         </form>
                     </center>
@@ -92,21 +136,15 @@ include('include/header.php');
     </div>
 </div>
 
-<?php
- if (isset($_POST['submit'])) {
-            $fname = $_POST['fname'];
-            $lname = $_POST["lname"];
-            $email = $_POST["email"];
-            $tel = $_POST["telnmr"];
-            $adres = $_POST["adres"];
-            $gdate = $_POST["gdate"];
-            $adres = $_POST["adres"];
-            $adres = $_POST["adres"];
-            $adres = $_POST["adres"];
-            $adres = $_POST["adres"];
-			
-if (empty ($fname or $lname or 
-echo "Niet alle verplichte velden zijn ingevuld"
-
+<?php 
 include('include/footer.php');
 ?>
+
+
+
+
+
+
+
+
+
