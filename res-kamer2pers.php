49a92c7fd934edee1531240180f186963b913e4d
ywielage <32356416+ywielage@users.noreply.github.com>
2020-01-14 12:46:16 +0100

    Links toegevoegd aan de pagina's zodat de klant kan reserveren. V2

--- res-kamer2pers.php
+++ res-kamer2pers.php
@@ -15,7 +15,7 @@ include'include\header.php';
             <h2  style="text-align:center;">Kamer <?php echo $_GET['action']?>, 2-persoonskamer</h2>
             <img src="img/standaard2persoonskamerjpg.jpg" class="Image1pers">
             <p class="1persP" style="margin:0 auto;width: 75%;color:white;">De 2 persoonskamer bestaat uit een slaapkamer en een badkamer. De slaapkamer is voorzien van een 2 persoonsbed,
-                                                                             een nachtkastje met accessoires, en een televisie. De badkamer heeft een douche en een bad.</p>
+                                                                             een nachtkastje met accessoires, en een televisie. De badkamer heeft een douche en een bad.
                 <a href='res-gegevens.php' style='border:1px solid white;'>Reserveer deze kamer</a>
                 <a href='res-kameroverzicht.php.php' style='border:1px solid white;'>Kies een andere kamer</a></p><br>                                                                            
         </div>
