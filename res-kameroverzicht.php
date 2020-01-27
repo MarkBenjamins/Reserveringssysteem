f02ddda6b3c3ccc2792a07ec07a27ec9f4c1b1a9
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-01-27 09:21:40 +0100

    Update res-kameroverzicht.php
    -white space weggehaald
    - layout uitgeleind

--- res-kameroverzicht.php
+++ res-kameroverzicht.php
@@ -1,10 +1,10 @@
-<?php   
+<?php
 
-        
+
 $stylesheet = "kamers";
 include('include/header.php');
 
-if(!isset($_SESSION["res-home"])) {
+if (!isset($_SESSION["res-home"])) {
     //wegsturen    
     die("session not set");
 }
@@ -14,98 +14,94 @@ $aankomst = $_SESSION["res-home"]["aankomst"];
 $vertrek = $_SESSION["res-home"]["vertrek"];
 //var_dump($_SESSION["res-home"]);
 
+$conn = mysqli_connect("127.0.0.1", "root", "");
 
-    $conn = mysqli_connect("127.0.0.1","root","");
+if ($conn == FALSE) { // verbinden met de server
+    echo "Kan niet verbinden met de server";
+}
 
-        if ($conn == FALSE){ // verbinden met de server
-            echo "Kan niet verbinden met de server";
-        }
-
-        if (!$conn->select_db("sollestijn")){ // database selecteren
-            echo "Kan de database niet selecteren";
-        }
+if (!$conn->select_db("sollestijn")) { // database selecteren
+    echo "Kan de database niet selecteren";
+}
 ?>
-    <div class="container">
-        <?php  
-        
-        function createDateRange($startDate, $endDate, $format = "Y-m-d") { // Functie die alle dagen binnen een periode weergeeft
-            $begin = new DateTime($startDate);
-            $end = new DateTime($endDate);
+<div class="container">
+    <?php
 
-            $interval = new DateInterval('P1D'); // 1 Day
-            $dateRange = new DatePeriod($begin, $interval, $end);
+    function createDateRange($startDate, $endDate, $format = "Y-m-d")
+    { // Functie die alle dagen binnen een periode weergeeft
+        $begin = new DateTime($startDate);
+        $end = new DateTime($endDate);
 
-            $range = [];
-            foreach ($dateRange as $date) {
-                $range[] = $date->format($format);
+        $interval = new DateInterval('P1D'); // 1 Day
+        $dateRange = new DatePeriod($begin, $interval, $end);
+
+        $range = [];
+        foreach ($dateRange as $date) {
+            $range[] = $date->format($format);
         }
         array_push($range, $endDate);
         return $range;
-        }
-        
-            //$arrayAlleDagen = createDateRange($aankomst, $vertrek);
-        
-            if ($stmt = $conn->prepare("SELECT * FROM kamer")){ // Haalt alle reserveringen uit de db
-                if ($stmt->execute()){
-                    if ($stmt->bind_result($id,$type, $omschrijving, $beschikbaar))
-                    {
-                        echo "<h2>Deze kamers zijn vrij</h2>";
-                        while ($row = $stmt->fetch()) { // Fetched alle data
-                            
-                            if($kamerKeuze == "Eenpersoonskamer") {
-                                //show all
-                                showKamer($id, $type, $omschrijving, $beschikbaar);
-                            } else if($kamerKeuze == "Tweepersoonskamer") {
-                                //Show 2, 3
-                                if($type == 2 || $type == 3) {
-                                    showKamer($id, $type, $omschrijving, $beschikbaar);
-                                }
-                            } else if($kamerKeuze == "Vierpersoonskamer") {
-                                //Show 3
-                                if($type == 3) {
-                                    showKamer($id, $type, $omschrijving, $beschikbaar);
-                                }
-                            }
+    }
+
+    //$arrayAlleDagen = createDateRange($aankomst, $vertrek);
+
+    if ($stmt = $conn->prepare("SELECT * FROM kamer")) { // Haalt alle reserveringen uit de db
+        if ($stmt->execute()) {
+            if ($stmt->bind_result($id, $type, $omschrijving, $beschikbaar)) {
+                echo "<h2>Beschikbare kamers</h2>";
+                while ($row = $stmt->fetch()) { // Fetched alle data
+
+                    if ($kamerKeuze == "Eenpersoonskamer") {
+                        //show all
+                        showKamer($id, $type, $omschrijving, $beschikbaar);
+                    } else if ($kamerKeuze == "Tweepersoonskamer") {
+                        //Show 2, 3
+                        if ($type == 2 || $type == 3) {
+                            showKamer($id, $type, $omschrijving, $beschikbaar);
                         }
-                    } else {
-                        echo "Bind failed";
+                    } else if ($kamerKeuze == "Vierpersoonskamer") {
+                        //Show 3
+                        if ($type == 3) {
+                            showKamer($id, $type, $omschrijving, $beschikbaar);
+                        }
                     }
-                } else {
-                    echo "Execute failed";
                 }
             } else {
-                echo "Prepare failed";
+                echo "Bind failed";
             }
-            
-
-
-        ?>
-    </div>
+        } else {
+            echo "Execute failed";
+        }
+    } else {
+        echo "Prepare failed";
+    }
+    ?>
+</div>
 <?php
 include('include/footer.php');
 
+function showKamer($id, $type, $omschrijving, $beschikbaar)
+{
 
-function showKamer($id, $type, $omschrijving, $beschikbaar) {
-        
-        if($type == 1) {
-            $kamer = "Eenpersoons kamer";
-            $url = "res-kamer1pers.php";
-        } else if($type == 2) {
-            $kamer = "Tweepersoons kamer";
-            $url = "res-kamer2pers.php";
-        } else {
-            $kamer = "Vierpersoons kamer";
-            $url = "res-kamer4pers.php";
-        }
-        echo "<div class='kamer'>";
-        echo "<p>".$kamer."</p>";
-        echo "<p>".$omschrijving."</p>";
-        echo "
-        <form method='POST' action='".$url."'>
-            <input type='hidden' name='id' value='".$id."'>
-            <input type='submit' name='kamersubmit' value='Ga naar kamer'>
+    if ($type == 1) {
+        $kamer = "Eenpersoons kamer";
+        $url = "res-kamer1pers.php";
+    } else if ($type == 2) {
+        $kamer = "Tweepersoons kamer";
+        $url = "res-kamer2pers.php";
+    } else {
+        $kamer = "Vierpersoons kamer";
+        $url = "res-kamer4pers.php";
+    }
+    echo "<div class='kamer'>";
+    echo "<p>" . $kamer . "</p>";
+    echo "<p>" . $omschrijving . "</p>";
+    echo "
+        <form class='form' method='POST' action='" . $url . "'>
+            <input type='hidden' name='id' value='" . $id . "'>
+            <input type='submit' class='btn mark-btn'name='kamersubmit' value='Ga naar kamer'>
         </form>
        ";
-       echo "</div>";
+    echo "</div>";
 }
 ?>
