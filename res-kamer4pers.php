2049d4674cf35ae691e2300847a2828e42dffac9
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-01-27 14:53:10 +0100

    kamer huisstyle update
    In efficente code verwijdert
    var_dumps uitgecomment
    
    opmaak en aangepast en tekst geupdate op de pagina

--- res-kamer4pers.php
+++ res-kamer4pers.php
@@ -2,38 +2,32 @@
 $stylesheet = "kamers";
 include 'include/header.php';
 ?>
+
 <!DOCTYPE HTML>
 <html>
-
-<head>
-    <title></title>
-</head>
-
 <body>
     <div id="container">
         <?php
         if (isset($_POST["kamersubmit"])) {
             $id = $_POST["id"];
-            var_dump($id);
-        } else {
-            echo "no";
+        //     var_dump($id);
+        // } else {
+        //     echo "no";
         }
         ?>
-
-        <h2 style="text-align:center;">Kamer, 1-persoonskamer</h2>
+        <h2 style="text-align:center;">Kamer, 4-persoonskamer</h2>
         <img src="img/1perskamer.jpg" class="Image1pers">
-        <p class="1persP" style="margin:0 auto;width: 75%;color:white;">Deze 1 persoonskamer bestaat uit een slaapkamer en een badkamer. De slaapkamer is voorzien van een 1 persoons bed, een nachtkastje met accessoires, en een televisie.
+        <p class="1persP" style="margin:0 auto;width: 75%;color:white;">Deze 4 persoonskamer bestaat uit een slaapkamer en een badkamer. De slaapkamer is voorzien van twee maal een 2 persoons bed, een nachtkastje met accessoires, en een televisie.
             De badkamer is voorzien van een douche en een bad<br><br>
             <form method='POST' action='res-gegevens.php'>
                 <input type='hidden' name='id' value='<?php echo $id; ?>'>
-                <input type='submit' value='Reserveer deze kamer'>
+                <input type='submit' class='btn mark-btn' value='Reserveer deze kamer'>
             </form>
 
-            <a href='res-kameroverzicht.php' style='border:1px solid white;'>Kies een andere kamer</a>
+            <a href='res-kameroverzicht.php' class='btn mark-btn' >Terug</a>
         </p><br>
     </div>
 </body>
-
 </html>
 
 <?php
