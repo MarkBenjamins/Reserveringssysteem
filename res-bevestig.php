35fdefc5db0b97cdc2e756544638bb1f61f29a91
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-01-29 10:21:42 +0100

    update
    Letter aanpassing

--- res-bevestig.php
+++ res-bevestig.php
@@ -1,37 +1,36 @@
-<?php 
+<?php
 $stylesheet = "betalen";
-include "include/header.php"; 
+include "include/header.php";
 //gemaakt door iedereen
 ?>
-
 <div class="container-fluid">
     <div class="row">
         <div class="col-4">
         </div>
         <div class="col-4">
-			<center><h1>Betaald</h1>	
+            <center>
+                <h1>Betaald</h1>
+            </center>
             <?php //Controle en melding van de betaalmethode
-                if (isset($_POST["methodesubmit"])){
-                    if ($_POST["methode"] == "paypal"){
-                        echo "<p class='tekstkleur'>Bedankt voor het betalen met PayPal.</p>";
-                    }
-                    elseif ($_POST["methode"] == "iDeal"){
-                        echo "<p class='tekstkleur'>Bedankt voor het betalen met IDeal.</p>";
-                    }
-                    elseif ($_POST["methode"] == "creditcard"){
-                        echo "<p class='tekstkleur'>Bedankt voor het betalen met creditcard.</p>";
-                    }
-                    else {
-                        echo "Error onbekende betaalmethode.";
-                    }
+            if (isset($_POST["methodesubmit"])) {
+                if ($_POST["methode"] == "paypal") {
+                    echo "<p class='tekstkleur'>Bedankt voor het betalen met PayPal.</p>";
+                } elseif ($_POST["methode"] == "iDeal") {
+                    echo "<p class='tekstkleur'>Bedankt voor het betalen met IDeal.</p>";
+                } elseif ($_POST["methode"] == "creditcard") {
+                    echo "<p class='tekstkleur'>Bedankt voor het betalen met creditcard.</p>";
+                } else {
+                    echo "Error onbekende betaalmethode.";
                 }
-                else {
-                    header("Location: res-betaling.php");
-                }
+            } else {
+                header("Location: res-betaling.php");
+            }
             ?>
-			<form method="POST" action="index.php">
-                <input type="submit" class="btn btn-outline-danger" value="print bevestiging" name="methodesubmit">
-            </form></center>		
+            <center>
+                <form method="POST" action="index.php">
+                    <input type="submit" class="btn btn-outline-danger" value="Print bevestiging" name="methodesubmit">
+                </form>
+            </center>
         </div>
         <div class="col-4">
         </div>
