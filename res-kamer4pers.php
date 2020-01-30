89f3f399fce5578e652f3f2308b73131e1b8fd1d
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-01-30 15:56:20 +0100

    Update res-kamer4pers.php
    pagina opgeschoond
    standaard tags voor html zijn niet nodig i.v.m. de include

--- res-kamer4pers.php
+++ res-kamer4pers.php
@@ -2,34 +2,24 @@
 $stylesheet = "kamers";
 include 'include/header.php';
 ?>
-
-<!DOCTYPE HTML>
-<html>
-<body>
-    <div id="container">
-        <?php
-        if (isset($_POST["kamersubmit"])) {
-            $id = $_POST["id"];
-        //     var_dump($id);
-        // } else {
-        //     echo "no";
-        }
-        ?>
-        <h2 style="text-align:center;">Kamer, 4-persoonskamer</h2>
-        <img src="img/4perskamer.jpg" class="Image1pers">
-        <p class="1persP" style="margin:0 auto;width: 75%;color:white;">Deze 4 persoonskamer bestaat uit een slaapkamer en een badkamer. De slaapkamer is voorzien van twee maal een 2 persoons bed, een nachtkastje met accessoires, en een televisie.
-            De badkamer is voorzien van een douche en een bad<br><br>
-            <form method='POST' action='res-gegevens.php'>
-                <input type='hidden' name='id' value='<?php echo $id; ?>'>
-                <input type='submit' class='btn mark-btn' value='Reserveer deze kamer'>
-            </form>
-
-            <a href='res-kameroverzicht.php' class='btn mark-btn' id='mrg5' >Terug</a>
-        </p><br>
-    </div>
-</body>
-</html>
-
+<div id="container">
+    <?php
+    if (isset($_POST["kamersubmit"])) {
+        $id = $_POST["id"];
+    }
+    ?>
+    <h2 style="text-align:center;">Kamer, 4-persoonskamer</h2>
+    <img src="img/4perskamer.jpg" class="Image1pers">
+    <p class="1persP" style="margin:0 auto;width: 75%;color:white;">Deze 4 persoonskamer bestaat uit een slaapkamer en een badkamer. De slaapkamer is voorzien van twee maal een 2 persoons bed, een nachtkastje met accessoires, en een televisie.
+        De badkamer is voorzien van een douche en een bad<br><br>
+        <form method='POST' action='res-gegevens.php'>
+            <input type='hidden' name='id' value='<?php echo $id; ?>'>
+            <input type='submit' class='btn mark-btn' value='Reserveer deze kamer'>
+        </form>
+        <a href='res-kameroverzicht.php' class='btn mark-btn' id='mrg5' >Terug</a>
+    </p>
+    <br>
+</div>
 <?php
 include 'include\footer.php';
 ?>
